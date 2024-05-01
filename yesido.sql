ed5d86df1753c488976527095a752a3f39392de9
Coles, Liam Karac <liamcole@iu.edu>
2024-05-01 14:54:38 -0400

    database

--- yesido.sql
+++ yesido.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 29, 2024 at 09:37 PM
+-- Generation Time: May 01, 2024 at 06:54 PM
 -- Server version: 5.7.24
 -- PHP Version: 8.0.1
 
@@ -51,6 +51,13 @@ CREATE TABLE `cart` (
   `c_id` int(11) DEFAULT NULL,
   `qty` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`p_id`, `ip_add`, `c_id`, `qty`) VALUES
+(2, '::1', 3, 1);
 
 -- --------------------------------------------------------
 
@@ -270,7 +277,7 @@ ALTER TABLE `shoots`
 -- Constraints for table `cart`
 --
 ALTER TABLE `cart`
-  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`p_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`p_id`) REFERENCES `shoots` (`shoot_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`c_id`) REFERENCES `customer` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
